691b3a938cd91ac781d70af488faff375deef89a
sreissig-git <reissisn@mail.uc.edu>
2026-06-18 00:12:49 -0400

    Task completed

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+//Class TheaterKiosk
+//  main()
+//      num personAge = 0
+//      output "Please enter your age: "
+//      input personAge
+//      if (personAge >= 21)
+//          output "Here is your paper wristband"
 
 public class TheaterKiosk {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int personAge = 0;
+        System.out.println("Please enter your age: ");
+        if (in.hasNextInt()) {
+            personAge = in.nextInt();
+            if (personAge >= 21) {
+                System.out.println("Here is your paper wristband");
+            }
+        }
+    }
 }
